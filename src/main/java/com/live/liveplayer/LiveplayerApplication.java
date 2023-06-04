d5c01dab1a1439bab8e40824e452ea261b5c1447
honggang.liu <honggang.liu@shopee.com>
2023-06-04 12:52:10 +0800

    Initial commit

---
+++ src/main/java/com/live/liveplayer/LiveplayerApplication.java
@@ -0,0 +1,13 @@
+package com.live.liveplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveplayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveplayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/live/liveplayer/LiveplayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.live.liveplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveplayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
